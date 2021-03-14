fc397f70ee3ba9a188ea2ca2fcb7025eb91eee48
abdelghanizerrouki99 <abdelghanizerrouki99@gmail.com>
2021-03-14 12:41:01 +0100

    Third commit

--- scratch.py
+++ scratch.py
@@ -10,24 +10,34 @@ components.html(
 <html>
     <head>
         <title>Your Website</title>
-        <script type="text/javascript" src="https://hosted.us.uneeq.io/deploy/85a5816e-8b9a-4da9-8aff-9bca9b8c8086"></script>
 
     </head>
     <body>
-        Your website.
+     <script type="text/javascript" src="https://hosted.us.uneeq.io/deploy/85a5816e-8b9a-4da9-8aff-9bca9b8c8086"></script>
     </body>
 </html>
-    """, width= 200, height= 200
+    """, width= 600, height= 800
 )
 
 components.html(
-    """
-    <iframe
-    allow="microphone;"
-    width="350"
-    height="430"
-    src="https://console.dialogflow.com/api-client/demo/embedded/2f250ef2-627f-48f3-9ae7-33336d499aaa">
-</iframe>
- """, width= 2000, height= 2000
+  """
+
+<html>
+    <head>
+        <title>Your Website</title>
+
+    </head>
+    <body style="display:flex;display-direction:row;justify-content:flex-end;">
+                <iframe
+                        allow="microphone;"
+                        width="350"
+                        height="430"
+                        src="https://console.dialogflow.com/api-client/demo/embedded/2f250ef2-627f-48f3-9ae7-33336d499aaa">
+                 </iframe>
+        </div>
+    </body>
+</html>
+    """, width= 1100, height= 500
 )
+
 
