ba794610c4b9c02ae2ebc3868f76afdb7a61bd76
abdelghani99 <58135724+abdelghani99@users.noreply.github.com>
2021-04-20 12:31:33 +0200

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -89,7 +89,7 @@ def main():
                                </script>
                            </body>
                          </html>
-                               """, width= 600, height= 600
+                               """, width= 700, height= 800
 		
 			
 )
